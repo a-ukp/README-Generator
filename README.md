9b325893ceba4104f3a54998d98085f438d34bd7
Anna Phung <uyenkphung@gmail.com>
2021-05-01 14:00:39 -0500

    updated readme file

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
-# README-Generator
+# README Generator
+
+## Description
+This project is a node.js application that helps the user create a README file from their terminal. 
+
+### Preview
+
+### Video Demonstration
+* [README Generator Demonstration]()
+
+---
+
+## Installation
+* [Node.js](https://nodejs.org/en/download/)
+* [Inquirer Package](https://www.npmjs.com/package/inquirer)
+* `npm install`
+
+---
+
+## Usage
+Inside terminal, run `node index.js` to deploy the application. Answer the questions accordingly to how you want it to appear in your file. If you'd like to leave the category blank, press `Enter` without typing an answer. A new README file will be created within your index.js folder.
+
+---
+
+## Credits
+* [Markdown License Badges](https://gist.github.com/lukas-h/2a5d00690736b4c3a7ba)
--- index.js
+++ index.js
@@ -1,46 +1,69 @@
 // TODO: Include packages needed for this application
-const generateMarkdown = require("./utils/generateMarkdown")
+const generateMarkdown = require("./utils/generateMarkdown");
+
+const inquirer = require("inquirer");
+const fs = require("fs");
+const util = require("util");
+const writeFileAsync = util.promisify(writeToFile);
 
 // TODO: Create an array of questions for user input
 const questions = [
     {
         type: 'input',
-        name: 'github-name',
-        message: 'What is your Github name?',
+        name: 'title',
+        message: 'What is the title of your project?',
     },
     {
         type: 'input',
-        name: 'github-name',
-        message: 'What is your Github name?',
+        name: 'description',
+        message: 'Briefly describe your project.',
     },
     {
         type: 'input',
-        name: 'github-name',
-        message: 'What is your Github name?',
+        name: 'installation',
+        message: 'What is required for installation to use your project?',
     },
     {
         type: 'input',
-        name: 'github-name',
-        message: 'What is your Github name?',
+        name: 'usage',
+        message: 'Include instructions and examples to use your application.',
     },
     {
         type: 'input',
-        name: 'github-name',
-        message: 'What is your Github name?',
+        name: 'credits',
+        message: 'Mention any collaborators, tutorials, or third-party apps used to create your project.',
     },
     {
         type: 'input',
-        name: 'github-name',
-        message: 'What is your Github name?',
+        name: 'features',
+        message: 'If your project has a lot of features, list them here.',
     },
 ];
 
 // TODO: Create a function to write README file
 // function writeToFile(fileName, data) {}
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, err => {
+        if (err) {
+            return console.log(err);
+        }
+
+        console.log("README.md file successfully created!")
+    });
+};
 
 // TODO: Create a function to initialize app
-function init() {prompt(questions).then(answers => generateMarkdown(answers))}
+async function init(){
+    try {
+        const userAnswers = await inquirer.prompt(questions);
+        
+        const markdown = generateMarkdown(userAnswers);
+
+        await writeFileAsync('README.md', markdown);
+    } catch (error) {
+        console.log(error);
+    }
+};
 
 // Function call to initialize app
 init();
